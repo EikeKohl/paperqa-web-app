499716bf8ea67c10a4d19b0cb5b721aeb6c9bf58
Eike Kohlmeyer <eike.kohlmeyer@verbraucherritter.de>
2023-07-01 13:21:50 +0200

    Fix and split shell deployment script

--- push_to_ecr.sh
+++ push_to_ecr.sh
@@ -2,8 +2,6 @@
 PROJECT_NAME=paperqa
 DOCKER_REGISTRY=YOUR_DOCKER_REGISTRY
 IMAGE_VERSION=1
-ECS_CLUSTER_NAME=paperqa-cluster
-ECS_SERVICE_NAME=paperqa-service
 
 aws ecr get-login-password | docker login --username AWS --password-stdin $DOCKER_REGISTRY
 docker build \
@@ -11,4 +9,3 @@ docker build \
 -t $DOCKER_REGISTRY/$PROJECT_NAME:latest .
 docker push $DOCKER_REGISTRY/$PROJECT_NAME:$IMAGE_VERSION
 docker push $DOCKER_REGISTRY/$PROJECT_NAME:latest
-aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME --force-new-deployment
